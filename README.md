2de55e4b378b5ea2eb9221b9fa88e2d33e48514b
sbrb <info.sbriju@gmail.com>
2023-01-19 14:09:23 +0530

    s

---
+++ README.md
@@ -0,0 +1,3 @@
+# digital-clock
+Using HTML, CSS, JavaScript.
+<img src='img.png' width='100%' height='100%'>
---
+++ script.js
@@ -0,0 +1,26 @@
+function showTime() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var session = 'AM';
+    if (h == 0) {
+        h = 12;
+    }
+    if (h > 12) {
+        h -= 12
+        session = 'PM';
+    }
+    if (h < 10) {
+        h = '0' + h;
+    }
+    if (m < 10) {
+        m = '0' + m;
+    }
+    if (s < 10) {
+        s = '0' + s;
+    }
+    document.getElementById('digitalClock').innerHTML = h + ':' + m + ':' + s + '' + session;
+    setTimeout(showTime, 1000);
+}
+showTime();
